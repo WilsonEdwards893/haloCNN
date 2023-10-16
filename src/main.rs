c0c79137e30a134a2576bb93e4ef2726b9d2200d
ouyang fang <ouyangfang12345@163.com>
2023-10-16 08:35:16 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,36 @@
-use halo2_proofs::{
-    arithmetic::Field,
-    circuit::{layouter::TableLayouter, Cell, Value, Chip, Layouter},
-    dev::{MockProver, VerifyFailure},
-    pasta::Fp,
-    plonk::{
-        Advice, Assignment, Circuit, Column, ConstraintSystem, Error, Expression, Fixed,
-        Selector
-    },
-    poly::Rotation,
-};
+use halo2_proofs::{pasta::Fp, circuit::Value, dev::MockProver};
 
-trait MatrixInstructions<F: Field>: Chip<F> {
-    /// Variable representing a matrix.
-    type Matrix;
-    // Variable representing a cell.
-    type Cell;
-    // Loads a matrix into the circuit as a private input.
-    fn load_private(&self, layouter: impl Layouter<F>, m: Vec<Vec<Value<F>>>) -> Result<Self::Matrix, Error>;
-    // Loads a matrix into the circuit as a public input.
-    fn load_public(&self, layouter: impl Layouter<F>, m: Vec<Vec<Value<F>>>) -> Result<Self::Matrix, Error>;
-    // Converts a matrix into a vector.
-    fn to_vector(&self, layouter: impl Layouter<F>, m: Self::Matrix) -> Result<Vec<Self::Cell>, Error>;
-    // Converts a vector into a matrix.
-    fn to_matrix(&self, layouter: impl Layouter<F>, v: Vec<Self::Cell>) -> Result<Self::Matrix, Error>;
-    // Returns the maximum value of a vector.
-    fn max(&self, layouter: impl Layouter<F>, v: Vec<Self::Cell>) -> Result<Self::Cell, Error>;
-}
-
-pub struct MatrixConfig {
-    // The column where the matrix elements are stored.
-    pub element_col: Column<Advice>,
-    // The column where the matrix indices are stored (optional).
-    pub index_col: Option<Column<Fixed>>,
-    // The selector for the conversion gate.
-    pub conversion_selector: Selector,
-    // The selector for the max gate.
-    pub max_selector: Selector,
-    // The selector for the output gate.
-    pub output_selector: Selector,
-}
-
-pub struct MatrixLoaded {
-    // The copy constraint for the element column.
-    pub element_copy: Copy,
-}
-
-pub struct MatrixChip<F: Field> {
-    config: MatrixConfig,
-}
-
-impl<F: Field> Chip<F> for MatrixChip<F> {
-    type Config = MatrixConfig;
-    type Loaded = MatrixLoaded;
-    
-    fn config(&self) -> &Self::Config {
-        &self.config
-    }
-
-    fn loaded(&self) -> &Self::Loaded {
-        &()
-    }
-}
 
 
 
 fn main() {
-    
+        // The number of rows in our circuit cannot exceed 2^k. Since our example
+    // circuit is very small, we can pick a very small value here.
+    let k = 4;
+
+    // Prepare the private and public inputs to the circuit!
+    let constant = Fp::from(7);
+    let a = Fp::from(2);
+    let b = Fp::from(3);
+    let c = constant * a.square() * b.square();
+
+    // Instantiate the circuit with the private inputs.
+    let circuit = MyCircuit {
+        constant,
+        a: Value::known(a),
+        b: Value::known(b),
+    };
+
+    // Arrange the public input. We expose the multiplication result in row 0
+    // of the instance column, so we position it there in our public inputs.
+    let mut public_inputs = vec![c];
+
+    // Given the correct public input, our circuit will verify.
+    let prover = MockProver::run(k, &circuit, vec![public_inputs.clone()]).unwrap();
+    assert_eq!(prover.verify(), Ok(()));
+
+    // If we try some other public input, the proof will fail!
+    public_inputs[0] += Fp::one();
+    let prover = MockProver::run(k, &circuit, vec![public_inputs]).unwrap();
+    assert!(prover.verify().is_err());
 }
