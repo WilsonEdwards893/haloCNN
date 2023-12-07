72ebbb315acf1a242e4d8f17d30abf3cb7e31135
ouyang fang <ouyangfang12345@163.com>
2023-12-07 10:25:37 +0800

    12/7

--- src/convcircuit.rs
+++ src/convcircuit.rs
@@ -10,6 +10,37 @@ use halo2_test::convchip::{ConvChip, ConvChipConfig};
 struct ConvCircuit<F: Field> {
     constant: F,
     input:Matrix<Value<F>>,
-    kernel: Matrix<Value<F>>,
+    filter: Matrix<Value<F>>,
     bias: Vec<Value<F>>,
+}
+
+impl<F: Field> Circuit<F> for ConvCircuit<F> {
+    // the config of Convolution layer
+    type Config = ConvChipConfig;
+
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        Self::default()
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        // We create the four advice columns that ConvChip uses for I/O.
+        let advice = [meta.advice_column();4];
+
+        // We also need an instance column to store public inputs.
+        let instance = meta.instance_column();
+ 
+        // Create a fixed column to load constants.
+        let constant = meta.fixed_column();
+
+        let filter_size: usize = Self::filter.iter().map(Vec::len).sum();
+        ConvChip::configure(meta, advice, instance, constant, filter_size)
+    }
+
+    fn synthesize(&self, 
+        config: Self::Config, 
+        layouter: impl halo2_proofs::circuit::Layouter<F>) -> Result<(), halo2_proofs::plonk::Error> {
+        todo!()
+    }
 }
